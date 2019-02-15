b57da64d429805206cdef0aa3cd7f385d89b6506
mthehorrible <mthehorrible@pm.me>
2019-02-15 18:56:17 -0500

    Removed test file

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-    mkdir -p ~/.config;
-    mkdir -p ~/.config/xfce4;
-    mkdir -p ~/.config/xfce4/xfconf;
-    mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml;
-    cp ./settings/thunar.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml;
